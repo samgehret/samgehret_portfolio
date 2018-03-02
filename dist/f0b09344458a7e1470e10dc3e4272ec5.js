3bc20da431001fee1897ff07950991826c7c5d18
Sam Gehret <sam.gehret@gmail.com>
2018-03-02 14:04:49 -0500

    made explicit landing page

--- dist/f0b09344458a7e1470e10dc3e4272ec5.js
+++ dist/f0b09344458a7e1470e10dc3e4272ec5.js
@@ -138,7 +138,7 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"_css_loader":9}],20:[function(require,module,exports) {
+},{"_css_loader":9}],26:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '61563' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '53883' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -261,5 +261,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[20])
+},{}]},{},[26])
 //# sourceMappingURL=/dist/f0b09344458a7e1470e10dc3e4272ec5.map
